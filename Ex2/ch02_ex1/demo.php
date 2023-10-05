b0b4c1765cb43858ad0b9b622ca90ab345e99492
OhDani <tranngoc2k41511@gmail.com>
2023-10-05 22:44:46 +0700

    Add

---
+++ Ex2/ch02_ex1/demo.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
