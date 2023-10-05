b681a54da862391db2690fb50ceeceb496f52c6f
OhDani <tranngoc2k41511@gmail.com>
2023-10-05 22:51:58 +0700

    Remove all

--- Ex2/ch02_ex1/demo.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
